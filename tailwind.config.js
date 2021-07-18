f3834e2227d4f81f970b9e442403e5bec62b389b
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-17 22:33:02 -0700

    added theme to tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const colors = require('tailwindcss/colors')
 // tailwind.config.js
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      green: colors.green,
+      gray: colors.gray,
+      blueGray: colors.blueGray,
+      white: colors.white,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
