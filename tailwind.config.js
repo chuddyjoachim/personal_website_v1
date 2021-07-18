436ade6fff7bd37d0f8f8e70967c156f86d302c9
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-18 07:42:08 -0700

    feat(animation): added coustom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 2s linear infinite',
+      },
+    },
     colors: {
       green: colors.green,
       gray: colors.gray,
