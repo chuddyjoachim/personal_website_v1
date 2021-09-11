3d747c774bb7a495bec3b5a737bdc8917b61e54c
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-09-10 18:17:55 -0700

    add portfolio tab context and provider

---
+++ context/portfolioTabContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, FC, useState } from 'react'
+import { pageTabContextType, pageTabstype } from '../lib/types'
+
+export const pTabContext = createContext<pageTabContextType>({
+  portfolioTabs: 'dev',
+  setPortfolioTab: () => {},
+})
+
+const PTabContextProvider: FC = ({ children }) => {
+
+  let initioalValue = 'dev' as pageTabstype
+
+  const [portfolioTabs, setPortfolioTab] = useState<pageTabstype>(() => {
+    return initioalValue
+  })
+
+  return (
+    <pTabContext.Provider value={{ portfolioTabs, setPortfolioTab }}>
+      {children}
+    </pTabContext.Provider>
+  )
+}
+
+export default PTabContextProvider
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import ThemeContextProvider from '../context/ThemeContext'
+import PTabContextProvider from '../context/portfolioTabContext'
 import { AnimateSharedLayout } from 'framer-motion'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
+      <PTabContextProvider>
       <AnimateSharedLayout>
         <Component {...pageProps} />
       </AnimateSharedLayout>
+      </PTabContextProvider>
     </ThemeContextProvider>
   )
 }
