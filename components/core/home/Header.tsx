82ff9b8da200306b48140b53ffb68a1bb71b1d17
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2024-01-03 13:38:51 +0100

    update: link styles and url

--- components/core/home/Header.tsx
+++ components/core/home/Header.tsx
@@ -8,13 +8,37 @@ import { AnimatePresence } from 'framer-motion'
 import PortfolioPop from '../../core/portfolio/PortfolioPop'
 
 const socials = [
-  { link: 'https://github.com/chuddyjoachim', icon: <FiGithub size={18} />, isExternal: true },
+  {
+    link: 'https://github.com/chuddyjoachim',
+    icon: <FiGithub size={18} />,
+    isExternal: true,
+    name: 'Github',
+  },
   //   { link: 'https://dribbble.com/chuddyjoachim', icon: <FiDribbble size={18} />, isExternal: true },
-  { link: 'https://twitter.com/chuddyjoachim', icon: <FiTwitter size={18} />, isExternal: true },
+  {
+    link: 'https://twitter.com/chuddyjoachim',
+    icon: <FiTwitter size={18} />,
+    isExternal: true,
+    name: 'Twitter',
+  },
   //   { link: 'https://www.behance.net/chuddyjoachim', icon: <FaBehance size={18} />, isExternal: true },
   {
     link: 'https://www.linkedin.com/in/chuddyjoachim/',
     icon: <FiLinkedin size={18} />,
+    isExternal: true,
+    name: 'LinkedIn',
+  },
+]
+
+const writtenArticles = [
+  {
+    url: 'https://dev.to/chuddyjoachim',
+    name: 'Dev.to(dev community)',
+    isExternal: true,
+  },
+  {
+    url: 'https://chuddyjoachim.medium.com/',
+    name: 'Medium',
     isExternal: true,
   },
 ]
@@ -24,8 +48,8 @@ const Header = () => {
 
   return (
     <div>
-      <div className="flex flex-col-reverse lg:flex-row transition-all items-center">
-        <div className="w-full lg:flex-[1.8] flex flex-col items-center lg:items-start space-y-4">
+      <div className="flex flex-col-reverse lg:flex-row transition-all lg:items-center">
+        <div className="w-full lg:flex-[1.8] flex flex-col items-start space-y-4">
           <div className="font-semibold text-5xl  text-center lg:text-left leading-tight mt-8 dark:text-slate-100 w-4/5 lg:w-auto">
             <h1 className="lg:leading-none leading-[1.1]">
               🖖Hey there!{' '}
@@ -34,10 +58,10 @@ const Header = () => {
               </span>
             </h1>
           </div>
-          <div className="py-7">
+          <div className="py-7 pb-1">
             <p className="text-sm md:text-base font-normal lg:text-base mb-3 text-gray-400 dark:text-gray-300 ">
-              A Product-oriented software engineer with over 4+ years of experience and deep focus
-              in frontend engineering,
+              A Product-oriented software engineer with experience and deep focus in frontend
+              engineering,
             </p>
             <p className="text-sm md:text-base font-normal lg:text-base mb-2 text-gray-400 dark:text-gray-300 ">
               In my journey so far I've Lead various frontend engineering roles with extensive
@@ -63,17 +87,43 @@ const Header = () => {
               working in a variety of culture and dynamic settings.
             </p>
           </div>
-          <div className="">
-            <div className=" flex flex-row justify-center lg:justify-start space-x-6 text-2xl font-light dark:text-white  opacity-40 dark:opacity-80">
+          <div className="py-2 pt-0">
+            <h3 className="texm-lg font-semibold  dark:text-slate-100">Writing:</h3>
+            <p className="text-sm md:text-base font-normal lg:text-base mb-2 text-gray-400 dark:text-gray-300 pt-1">
+              I try to write about tool and tecnologies i work with.
+            </p>
+            <ul className="list-disc list-inside space-y-1">
+              {writtenArticles?.map((link, index) => (
+                <li
+                  key={`${index}`}
+                  className="text-sm md:text-base font-normal lg:text-base mb-2 dark:text-gray-300 text-purple-x-300 hover:text-purple-x-400 underline"
+                >
+                  <Link to={link.url} isExternal={link.isExternal}>
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="pt-1">
+            <h3 className="texm-lg font-semibold  dark:text-slate-100">Socials:</h3>
+            <div className=" mt-1 flex flex-row justify-start space-x-6 text-2xl font-light dark:text-white ">
               {socials.map((s, i) => (
-                <div key={i}>
+                <div
+                  key={i}
+                  className="text-purple-x-300 dark:text-gray-300 last:after:border-0 after:border-r after:border-current after:pl-4 flex"
+                >
                   <Link to={s.link} isExternal={s.isExternal}>
-                    {s.icon}
+                    {/* {s.icon} */}
+                    <p className="text-sm md:text-base  font-normal lg:text-base  dark:text-gray-300 text-purple-x-300 hover:text-purple-x-400 underline">
+                      {s.name}
+                    </p>
                   </Link>
                 </div>
               ))}
             </div>
-            <div className="py-10">
+
+            <div className="py-10 pt-7">
               {/* <small className=" text-xl font-semibold underline underline-offset-2 dark:text-white text-center block">Explore:</small> */}
               <div className="flex gap-4 mt-3">
                 <div className="">
@@ -84,19 +134,7 @@ const Header = () => {
                     rightIcon={<FaCode />}
                     onClick={() => setIsOptionOpen((v) => !v)}
                   >
-                    Work
-                  </Button>
-                </div>
-                <div className="">
-                  <Button
-                    type="button"
-                    variant="outlined"
-                    size="md"
-                    className="font-bold select-none dark:text-gray-50 dark:border-gray-50"
-                    rightIcon={<PiNotepadBold size={18} />}
-                    onClick={() => setIsOptionOpen((v) => !v)}
-                  >
-                    Articles
+                    Explore Work & Projects
                   </Button>
                 </div>
               </div>
